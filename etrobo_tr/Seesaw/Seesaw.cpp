587df33e2c194d5ab45629daad02f7bf5440e3a2
garyo <surpace2479@gmail.com>
2019-09-11 10:46:27 +0900

    update: 昇段成功

--- etrobo_tr/Seesaw/Seesaw.cpp
+++ etrobo_tr/Seesaw/Seesaw.cpp
@@ -55,93 +55,59 @@ void Seesaw::run()
 
     // 減速
     case 1:
-        lineRun(1, 50, 0, 35);
-
-        // シーソーを検知
-        if (seesaw_dis > 0.4)
-        {
-            ev3_speaker_play_tone(262, 100);
-            m_seesaw_start_dis = m_guage->getRobotDis();
-            m_line_tracer->setPidParm(m_run_pid_param[1]);
-            m_sequence_num++;
-        }
-        break;
-
-    // シーソー検知まで減速
-    case 2:
-        lineRun(1, 30, 0, 35);
-
-        // シーソーを検知
-        if (abs(m_guage->getPitchVel()) > 150)
-        {
-            ev3_speaker_play_tone(262, 100);
-            m_seesaw_start_dis = m_guage->getRobotDis();
-            m_line_tracer->setPidParm(m_run_pid_param[2]);
-            start_time = m_clock.now();
-            m_sequence_num++;
-        }
-        break;
-
-    // 後進
-    case 3:
-        m_gyro.setOffset(-6);
-        lineRun(1, 0, 0, 35);
-        if (m_clock.now() - start_time > 5000)
-        {
-            ev3_speaker_play_tone(262, 100);
-            m_line_tracer->setPidParm(m_run_pid_param[3]);
-            m_sequence_num++;
-        }
-        break;
-
-    // 後進
-    case 4:
-        m_gyro.setOffset(0);
-        lineRun(1, -30, 0, 35);
-        if (m_guage->getRobotDis() - m_seesaw_start_dis < -0.35)
-        {
-            ev3_speaker_play_tone(262, 100);
-            m_sequence_num++;
-        }
-        break;
-
-    // 前進
-    case 5:
         lineRun(1, 80, 0, 35);
-        if (m_guage->getRobotDis() - m_seesaw_start_dis > 0)
-        {
-            ev3_speaker_play_tone(262, 100);
-            start_time = m_clock.now();
-            m_line_tracer->setPidParm(m_run_pid_param[3]);
-            m_sequence_num++;
-        }
-        break;
 
-    // 前進
-    case 6:
-        m_gyro.setOffset(-31);
-        lineRun(1, 90, 0, 35);
-        if (m_clock.now() - start_time > 200)
+        // シーソーを検知
+        // if (seesaw_dis > 0.57)
+        if (seesaw_dis > 0.25)
         {
-            ev3_speaker_play_tone(262, 100);
-            m_sequence_num++;
-        }
-        break;
-
-    // 前進
-    case 7:
-        m_gyro.setOffset(0);
-        lineRun(1, 95, 0, 35);
-        if (m_guage->getRobotDis() - m_seesaw_start_dis > 0.15)
-        {
+            ev3_led_set_color(LED_ORANGE);
             ev3_speaker_play_tone(262, 100);
             start_time = m_clock.now();
             m_sequence_num++;
         }
         break;
 
+    // // 減速
+    // case 2:
+    //     lineRun(1, 80, 0, 35);
+
+    //     // シーソーを検知
+    //     if (abs(m_guage->getPitchVel()) > 150)
+    //     {
+    //         ev3_led_set_color(LED_GREEN);
+    //         ev3_speaker_play_tone(262, 100);
+    //         m_seesaw_start_dis = m_guage->getRobotDis();
+    //         m_line_tracer->setPidParm(m_run_pid_param[1]);
+    //         m_sequence_num++;
+    //     }
+    //     break;
+
     // 前進
-    case 8:
+    case 2:
+        m_gyro.setOffset(-30);
+        lineRun(1, 100, 0, 35);
+        if (m_clock.now() - start_time > 150)
+        {
+            ev3_speaker_play_tone(262, 100);
+            m_sequence_num++;
+        }
+        break;
+
+    // 前進
+    case 3:
+        m_gyro.setOffset(-4);
+        lineRun(1, 130, 0, 35);
+        // if (m_guage->getRobotDis() - m_seesaw_start_dis > 0.15)
+        // {
+        //     ev3_speaker_play_tone(262, 100);
+        //     start_time = m_clock.now();
+        //     m_sequence_num++;
+        // }
+        break;
+
+    // 前進
+    case 4:
         lineRun(1, 0, 0, 35);
         if (m_clock.now() - start_time > 700)
         {
@@ -151,7 +117,7 @@ void Seesaw::run()
         break;
 
     // 前進
-    case 9:
+    case 5:
         lineRun(1, 20, 0, 35);
         break;
     default:
